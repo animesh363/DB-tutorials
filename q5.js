d0d1772ddd594c9b559f77302de8745c25bdf407
animesh363 <animeshanand86033@gmail.com>
2026-02-02 11:52:51 +0530

    addedf

---
+++ q5.js
@@ -0,0 +1,20 @@
+db.employees.find({department:"HR"}).count() //we will ge no of employee in HR department
+
+db.employees.find(
+    {department:{$eq:"HR"}} //equalto 
+)
+db.employees.find(
+    {department:{$gt:"HR"}} //greater than 
+)
+db.employees.find(
+    {department:{$gte:"HR"}} //greater than equal to
+)
+db.employees.find(
+    {department:{$ne:"HR"}} //not equal to
+)
+db.employees.find(
+    {department:{$lt:"HR"}} //less than
+)
+db.employees.find(
+    {department:{$lte:"HR"}} //less than equal to
+)
