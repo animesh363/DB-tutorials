1255062cf1bc4a0ed99e2d3bd56dd50a3cef7c99
animesh363 <animeshanand86033@gmail.com>
2026-02-03 12:19:36 +0530

    aggregate

--- q8.js
+++ q8.js
@@ -12,10 +12,30 @@ db.employees.find({},{_id:0, name:1}).sort({name:1})
 db.employees.find({},{_id:0,name:1}).collation({locale:'en',strength:2}).sort({name:1})
 
 
-db.employees.agregate(
+db.employees.aggregate(
     [
         {},
         {},
         {}
+    ]
+)
+db.employees.aggregate(
+    [
+        {$match:{department:"HR"}}
+    ]
+)
+db.employees.aggregate(
+    [
+        {$project:{_id:0, department:0, salary:0}} // if you dont want you write those
+    ]
+)
+db.employees.aggregate(
+    [
+        {$sort:{name:1}} 
+    ]
+)
+db.employees.aggregate(
+    [
+        {$limit:3}
     ]
 )
